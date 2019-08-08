6cf9d0db9a48b79e8d2fbd2438d153bfc6bcd98b
AVeryHandsomePerson <ksgtljh@126.com>
2019-08-08 17:32:53 +0800

    统计数对出现个数，并生成矩阵

--- src/main/java/com/cn/ben/utile/DemoUtile.java
+++ src/main/java/com/cn/ben/utile/DemoUtile.java
@@ -4,7 +4,11 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @Auther: ljh
@@ -15,13 +19,20 @@ public class DemoUtile {
     //读取文件
     public String NumberOf() throws IOException {
 
-        List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\dell\\.ssh\\id_rsa.pub"), StandardCharsets.UTF_8);
+        List<String> lines = Files.readAllLines(Paths.get("E:\\JinFengIDeaFile\\dzfile\\src\\main\\java\\com\\cn\\ben\\sd.log"), StandardCharsets.UTF_8);
         StringBuilder sb = new StringBuilder();
         for(String line : lines){
-            sb.append(line);
+            sb.append(line).append(",");
         }
-        String fromFile = sb.toString();
-        System.out.println(fromFile);
+        List<String> list =new ArrayList<>();
+        String [] str  =sb.toString().split(",");
+        for (int i = 0; i < str.length-1; i++) {
+            list.add("("+str[i]+","+str[i+1]+")");
+        }
+      Map<String,Long> map =  list.stream().flatMap(line -> Arrays.stream(line.split(" ")))
+                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+        ListStat.demoStat(map);
+
         return super.toString();
     }
 
