2fce24a92d38e2daadf64e77565c1967ac046f14
AVeryHandsomePerson <ksgtljh@126.com>
2019-11-21 16:04:59 +0800

    包含|数据 处理

--- src/main/java/com/cn/ben/controller/EgController.java
+++ src/main/java/com/cn/ben/controller/EgController.java
@@ -1,7 +1,11 @@
 package com.cn.ben.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -11,18 +15,33 @@ import java.util.Map;
  */
 @Controller
 public class EgController {
-    @RequestMapping(value="/", method = RequestMethod.GET)
-    public String passParam(Map <String, Object> map){
+    @RequestMapping("/")
+    public ModelAndView test() {
+        ModelAndView modelAndView = new ModelAndView("/image");
+        ArrayList arr = new ArrayList();
+        arr.add("50");
+        arr.add("#2dc6c8");
+        arr.add("瓜子");
+        List<ArrayList> result = new ArrayList<>();
+        result.add(arr);
 
-        map.put("message1", "Hello, Spring Boot!");
-
-        map.put("message2", "Hello, Spring Boot!");
-
-        User user = new User(18, "Bruce");
-
-        map.put("user", user);
-
-        return "result";
-
+//[50,"#2dc6c8","瓜子"], [100,"#b6a2dd", "花生"], [200,"#5ab1ee","土豆"], [700,"#d7797f","南瓜四号"], [70,"#d7797f","四号"]
+        modelAndView.addObject("result", result);
+        return modelAndView;
     }
+
 }
+//    @RequestMapping(value="/", method = RequestMethod.GET)
+//    public String passParam(Map <String, Object> map){
+//
+//        map.put("message1", "Hello, Spring Boot!");
+//
+//        map.put("message2", "Hello, Spring Boot!");
+//
+//        User user = new User(18, "Bruce");
+//
+//        map.put("user", user);
+//
+//        return "result";
+//
+//    }
