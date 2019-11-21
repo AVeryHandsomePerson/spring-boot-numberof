ac5de48d8860dd19de535cb644b6bf95198073c8
AVeryHandsomePerson <ksgtljh@126.com>
2019-11-21 16:48:24 +0800

    包含|数据 处理

--- src/main/java/com/cn/ben/controller/EgController.java
+++ src/main/java/com/cn/ben/controller/EgController.java
@@ -1,5 +1,7 @@
 package com.cn.ben.controller;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,19 +16,25 @@ import java.util.Map;
  * @Description:
  */
 @Controller
+@Slf4j
 public class EgController {
     @RequestMapping("/")
     public ModelAndView test() {
         ModelAndView modelAndView = new ModelAndView("/image");
-        ArrayList arr = new ArrayList();
-        arr.add("50");
+        ArrayList<Object> arr = new ArrayList();
+        arr.add(50);
         arr.add("#2dc6c8");
         arr.add("瓜子");
         List<ArrayList> result = new ArrayList<>();
         result.add(arr);
-
+        ArrayList<Object> arr1 = new ArrayList();
+        arr1.add(500);
+        arr1.add("#2dc6c8");
+        arr1.add("瓜子1");
+        result.add(arr1);
 //[50,"#2dc6c8","瓜子"], [100,"#b6a2dd", "花生"], [200,"#5ab1ee","土豆"], [700,"#d7797f","南瓜四号"], [70,"#d7797f","四号"]
-        modelAndView.addObject("result", result);
+        System.out.println(JSON.toJSONString(result));
+        modelAndView.addObject("result", JSON.toJSONString(result));
         return modelAndView;
     }
 
